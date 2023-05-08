943923bfb60c20a5131eb9b11adbed9c3dc2565e
Abdullah Sholi <s12027918@stu.najah.edu>
2023-05-08 13:26:16 +0300

    Adding Jacoco

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,7 +1,7 @@
         import java.util.ArrayList;
         import java.util.List;
 
-        import java.util.logging.Level;
+         import java.util.logging.Level;
         import java.util.logging.Logger;
         public class Admin {
 
